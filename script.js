89ae8649b9a46d492dd4230737b7acf876c49a03
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-07-27 15:59:01 -0400

    7/27: Assignment complete. Checking over work to make sure it is clean and couldn't be done better. All answers True in console.

--- script.js
+++ script.js
@@ -99,12 +99,11 @@ let letterVals = [ "v", "x", "r", "f", "p" ];
 
 function interleave(){
     let vals = [];
-    // Your answer here:
+    for (i = 0; i < letterVals.length; i++) {
+      vals[i] = letterVals[i] + numberVals[i];
+    }
+    return vals;
 }
-
-
-
-
 
 
 /*
@@ -116,5 +115,10 @@ let second = [ "think", "stand", "cheese", "break", "numinous", "mouse", "close"
 
 function union(){
     let same = [];
-    // Your answer here:
+    for (i = 0; i < first.length; i++) {
+      if (first[i] === second[i]) {
+        same.push(first[i]);
+      }
+    }
+    return same;
 }
